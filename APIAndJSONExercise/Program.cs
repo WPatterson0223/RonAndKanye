714f604682e6a778caa3856afbdf3535988bfb4b
Will Patterson <wlp223@aol.com>
2023-03-06 12:02:59 -0500

    exercise 1

---
+++ APIAndJSONExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace APIAndJSONExercise;
+class Program
+{
+    static void Main(string[] args)
+    {
+        var client = new HttpClient();
+
+        var ronSwanson = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+        var ye = "https://api.kanye.rest/";
+
+        for (int i = 0; i < 5; i++)
+        {
+            var ronResponse = client.GetStringAsync(ronSwanson).Result;
+            var yeResponse = client.GetStringAsync(ye).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+            var yeQuote = JObject.Parse(yeResponse).ToString().Replace('{', ' ').Replace('}', ' ').Trim();
+
+            Console.WriteLine($"Ron Swanson: {ronQuote}");
+            Console.WriteLine();
+
+            Console.WriteLine($"Kenye West: {yeQuote}");
+            Console.WriteLine();
+        }
+    }
+}
+
